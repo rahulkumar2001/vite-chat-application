878dc0b2fddcb90b3192e7c7250efdc7a2c39fc9
aadil raza <aadil18@navgurukul.org>
2024-02-13 19:55:08 +0530

    fix rendering issue and package versioning

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+import Join from "./components/Join.jsx";
+import Chat from "./components/Chat.jsx";
+
+const App = () => (
+    //    <h1>helldjsofjd os1dfhodsfhoi</h1>
+    <Router>
+        <Routes>
+            <Route path="/" element={<Join />} />
+            <Route path="/chat" element={<Chat />} />
+
+        </Routes>
+    </Router>
+)
+
+export default App
